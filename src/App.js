63c7a9713fbef4398658201f9e6dbd2feea62a5b
imjord <dean.jay.goat@gmail.com>
2021-11-11 13:18:52 -0700

    updated folders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Nav from './components/Nav'
-import LandingPage from './components/LandingPage';
-import Footer from './components/Footer';
-import Contact from './components/Contact';
-import Projects from './components/Projects';
+import Nav from './components/Nav/index'
+import LandingPage from './components/LandingPage/index';
+import Footer from './components/Footer/index';
 
 
 function App() {
