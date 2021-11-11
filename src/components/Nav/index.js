056ac5b6327d3d048fb1640abac0f67829369d95
imjord <dean.jay.goat@gmail.com>
2021-11-11 13:39:23 -0700

    sample header

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import './style.css';
 
 function Nav(){
     return(
-        <div>This is my Nav bar!</div>
+        <div className="nav-bar-container border border-top-0 border-dark">
+        <div className="left">
+            This is left side in container
+        </div>
+        <div className="right"> this is the right side of the container</div>
+        </div>
     )
 }
 
