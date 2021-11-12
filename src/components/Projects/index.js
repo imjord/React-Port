025b5e72da8e70f11d8f235800f63f2aac3c1280
imjord <dean.jay.goat@gmail.com>
2021-11-12 15:15:27 -0700

    working carousel

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,6 +2,9 @@
 import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import { Carousel } from 'react-responsive-carousel';
+import image1 from '../../assets/coinvrt.PNG'
+import image2 from '../../assets/wayo.PNG';
+import image3 from '../../assets/fooder.PNG';
 
 
 
@@ -9,15 +12,15 @@ function Projects(){
     return(
       <Carousel>
           <div>
-                    <img src="../../assets/coinvrt.PNG" />
+                    <img src={image1} alt="" />
                     <p className="legend">coinvrt</p>
                 </div>
                 <div>
-                    <img src="../../assets/wayo.PNG" />
+                    <img src={image2} alt="" />
                     <p className="legend">wayo</p>
                 </div>
                 <div>
-                    <img src="../../assets/fooder.PNG" />
+                    <img src={image3} alt="" />
                     <p className="legend">fooder</p>
                 </div>
       </Carousel>
