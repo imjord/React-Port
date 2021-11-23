9ca1c9134d371feed4a786b323236b6a7e3083f3
imjord <dean.jay.goat@gmail.com>
2021-11-23 12:13:36 -0700

    Added more projects with images

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -5,6 +5,10 @@ import { Carousel } from 'react-responsive-carousel';
 import image1 from '../../assets/coinvrt.PNG'
 import image2 from '../../assets/wayo.PNG';
 import image3 from '../../assets/fooder.PNG';
+import image4 from '../../assets/thoughts.png';
+import image5 from '../../assets/GUIguessinggame.PNG';
+import image6 from '../../assets/node-backend-notes.PNG';
+import image7 from '../../assets/runBuddy.PNG';
 
 
 
@@ -26,10 +30,26 @@ function Projects(){
                     <img src={image3} alt="" />
                     <p className="legend"><a className="link" href="https://powerful-crag-64537.herokuapp.com/" target="_blank">FOODER</a></p>
                 </div>
+                <div>
+                    <img src={image4} alt="" />
+                    <p className="legend"><a className="link" href="https://github.com/imjord/NOSQL-SOCIAL-NETWORK-API" target="_blank">Deep Thoughts</a></p>
+                </div>
+                <div>
+                    <img src={image5} alt="" />
+                    <p className="legend"><a className="link" href="https://github.com/imjord/GUI-Guessing-Game" target="_blank">Java GUI Guessing Game</a></p>
+                </div>
+                <div>
+                    <img src={image6} alt="" />
+                    <p className="legend"><a className="link" href="https://github.com/imjord/node-noteTaker" target="_blank">Note Taker Backend</a></p>
+                </div>
+                <div>
+                    <img src={image7} alt="" />
+                    <p className="legend"><a className="link" href="https://imjord.github.io/run-buddy/index.html" target="_blank">Run Buddy HTML</a></p>
+                </div>
                 
       </Carousel>
       <div className="description-container">
-      <p><i>These are 3 amazing projects that I created during my time at the U of U Coding Bootcamp. I was the sole backend developer for 2 out of the 3 projects.</i> </p>
+      <p><i>Here are some amazing projects that I created during my time at the U of U Coding Bootcamp. I was the sole backend developer for group projects like wayo and fooder.</i> </p>
       
       </div>
   
