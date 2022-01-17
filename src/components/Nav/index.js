3a9157d8a1a6de470e3ca117e1fb42a2e778c7e7
imjord <dean.jay.goat@gmail.com>
2022-01-17 14:43:44 -0700

    fixed hamburger should be working now

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -22,13 +22,18 @@ function Nav(){
              <li><a className="nav-link" href="/Projects">Projects </a></li>
              <li><a className="nav-link" href="/Skills">Skills </a></li>
              <li><a className="nav-link" href="/Resume">Resume </a></li>
-             <Menu className='udj' isOpen={ true }>
+
+             <i class="fas fa-bars">
+
+
+             <Menu className='fas'>
              <li><a  className="menu-item" href="/">Home </a></li>
              <li><a className="menu-item" href="/Projects">Projects </a></li>
              <li><a className="menu-item" href="/Skills">Skills </a></li>
              <li><a className="menu-item" href="/Resume">Resume </a></li>
-             <a onClick={ hamClick } className="menu-item--small" href="">Settings</a>
+             <a onClick={ hamClick } className="menu-item--small" href=""></a>
         </Menu>
+        </i>
 
 
              </ul></div>
