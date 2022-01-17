31173a7b540df1fbedeeb7d14f66c8507bc909db
imjord <dean.jay.goat@gmail.com>
2022-01-17 14:46:46 -0700

    added email to resume made more mobiile friendly on resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -6,9 +6,11 @@ import NoteIcon from '@mui/icons-material/Note';
 function Resume(){
     return(
         <div className="container">
+             
             <div className="left"><i>
-            Hey thank you for visiting my portfolio! If you would like to see my resume. Click the note!
+            Hey thank you for visiting my portfolio! If you would like to see my resume. Click the note! Email: Jordanharperjs@gmail.com
                </i></div>
+              
             <div className="right">
             <a href="https://docs.google.com/document/d/1T6D4e4ECXR3YcMbVqCPjSnIj26LHT9_vbyT3WdDVanw/edit?usp=sharing" target="_blank"><NoteIcon/></a>
             </div>
